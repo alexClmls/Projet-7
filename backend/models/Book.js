5c0e1dfdd36d850a3ccffd61d6c9f44be69b58a2
Alexandre Calmels <acalmels92@hotmail.fr>
2023-10-16 17:31:33 +0200

    mise en place du backend

---
+++ backend/models/Book.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const ratingSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  grade: { type: Number, required: true },
+});
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  ratings: [ratingSchema],
+  averageRating: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Book', bookSchema);
